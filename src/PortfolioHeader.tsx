4d67c76607f8740e0fb009644b52f4ead41cee0f
Linkazen <jpch2k4@gmail.com>
2026-09-23 10:12:22 +0100

    Added new body

---
+++ src/PortfolioHeader.tsx
@@ -0,0 +1,10 @@
+import "./PortfolioHeader.css";
+
+export default function PortfolioHeader() {
+  return (
+    <section>
+      <h1 id="folioTitle">Jonathan Hurst</h1>
+      <p>Game Engine Engineer</p>
+    </section>
+  );
+}
